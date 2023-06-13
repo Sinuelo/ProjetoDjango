1f6b64adafcf9431b2ceef36da0289103aa2c3c8
Sinuelo <papahet101@hotmail.com>
2023-06-13 14:45:31 -0300

    "Arrumando erro staticfiles"

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
+SECRET_KEY = config('SECRET_KEY')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-l%2-=ehg_=%la!s56@*-ko6kg00vzim*%a9fpl=2+&oot#8288"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
