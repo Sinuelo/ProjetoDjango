169e162d9b19ceec996e9e60722508c17e3fd52a
Sinuelo <papahet101@hotmail.com>
2023-06-15 14:03:31 -0300

    "tentando arrumar o banco de dados"

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -96,11 +96,12 @@ import os
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 if DATABASE_URL:
-    DATABASES['default']: dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
 
 
 
-
+    }
 
 
 # Password validation
