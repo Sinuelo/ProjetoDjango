ff3c477d4e2f870ce18182a06b1f460113dee97a
Sinuelo <papahet101@hotmail.com>
2023-06-23 16:26:56 -0300

    outro deploy

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -18,9 +18,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-with open('C:/Users/Lucas/Hashtag Treinamentos/ProjetoDjango/password.txt') as arquivo:
-    PGPASSWORD = arquivo.read().strip()
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -94,7 +91,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": 'railway',
         'USER': 'postgres',
-        'PASSWORD':PGPASSWORD ,
+        'PASSWORD': os.environ.get('PGPASSWORD'),
         'HOST': 'containers-us-west-183.railway.app',
         'PORT': '6542'
     }
