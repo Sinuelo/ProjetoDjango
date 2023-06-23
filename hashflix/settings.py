67f29b4df290079424331a2bcc4078566854d966
Sinuelo <papahet101@hotmail.com>
2023-06-23 16:33:55 -0300

    outro deploy

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": 'railway',
         'USER': 'postgres',
-        'PASSWORD': os.environ.get('PGPASSWORD'),
+        'PASSWORD':os.getenv('PGPASSWORD'),
         'HOST': 'containers-us-west-183.railway.app',
         'PORT': '6542'
     }
