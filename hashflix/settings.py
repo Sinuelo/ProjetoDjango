b700472d4a5e551be03b8690ef1b4578aae1b790
Sinuelo <papahet101@hotmail.com>
2023-06-23 15:49:52 -0300

    atualizando requirements.txt

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -90,11 +90,11 @@ WSGI_APPLICATION = "hashflix.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT'),
+        'NAME': os.getenv('PGDATABASE'),
+        'USER': os.getenv('PGUSER'),
+        'PASSWORD': os.getenv('PGPASSWORD'),
+        'HOST': os.getenv('PGHOST'),
+        'PORT': os.getenv('PGPORT'),
     }
 }
 
