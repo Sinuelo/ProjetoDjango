a993525f01079bedc0646b875edebbe1e0856fe5
Sinuelo <papahet101@hotmail.com>
2023-06-23 14:18:28 -0300

    "tentando arrumar o banco de dados"

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = "hashflix.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'ZdvZCSgXPugipDy5LFJ1',
+        'HOST': 'containers-us-west-183.railway.app',
+        'PORT': '6542'
     }
 }
 
